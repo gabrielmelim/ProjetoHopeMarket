417015aa77b90f1943e9a2a0e24b90675eacb2cf
Lucas Roberto de Oliveira Barcelos <lucasolbarcelos@gmail.com>
2022-02-25 14:52:19 -0300

    [@LucassRoberto Task: Criação do end point PostMapping e GetMapping]

--- app_hopemarket/src/main/java/org/generation/app_hopemarket/controller/ProdutoController.java
+++ app_hopemarket/src/main/java/org/generation/app_hopemarket/controller/ProdutoController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.generation.app_hopemarket.model.Produto;
 import org.generation.app_hopemarket.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +29,15 @@ public class ProdutoController {
     public ResponseEntity<List<Produto>> findAll(){
         return ResponseEntity.ok(repository.findAll());
 
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto>findByIdProdutos (@PathVariable Long id) {
+    	return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    }
+    
+    @PostMapping
+    public ResponseEntity<Produto> post (@RequestBody Produto produtos){
+    	return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produtos));
     }
 }
