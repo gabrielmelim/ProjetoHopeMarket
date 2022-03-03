8648b6a72d2012f02fbb8079d2c255329fe6de83
Fernando Porto <fernandogomes143@gmail.com>
2022-03-03 16:15:28 -0300

    @fernandoportodev [Task: Criado CarrinhoRepository com metodo busca id_compra]

--- app_hopemarket/src/main/java/org/generation/app_hopemarket/controller/CarrinhoController.java
+++ app_hopemarket/src/main/java/org/generation/app_hopemarket/controller/CarrinhoController.java
@@ -1,8 +1,10 @@
 package org.generation.app_hopemarket.controller;
 
 
-import org.generation.app_hopemarket.model.Produto;
+import org.generation.app_hopemarket.model.Carrinho;
+import org.generation.app_hopemarket.repository.CarrinhoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,37 +12,36 @@ import java.util.List;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/produtos")
+@RequestMapping("/carrinho")
 public class CarrinhoController {
-
 
         @Autowired
         private CarrinhoRepository repository;
 
         @GetMapping
-        public ResponseEntity<List<Produto>> findAll(){
+        public ResponseEntity<List<Carrinho>> findAllCarrinho(){
             return ResponseEntity.ok(repository.findAll());
 
         }
 
         @GetMapping("/{id}")
-        public ResponseEntity<Produto>findByIdProdutos (@PathVariable Long id) {
+        public ResponseEntity<Carrinho> findByIdCarrinho (@PathVariable Long id) {
             return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
         }
 
-        @GetMapping("/categoria/{categoria}")
-        public ResponseEntity <List<Produto>> findByCategoriaProduto (@PathVariable String categoria){
-            return ResponseEntity.ok(repository.findAllByCategoriaContainingIgnoreCase(categoria));
+        @GetMapping("/id_compras/{id_compras}")
+        public ResponseEntity<List<Carrinho>> findByIdComprasCarrinho (@PathVariable Long id_compras){
+            return ResponseEntity.ok(repository.findAllByIdComprasContainingIgnoreCase(id_compras));
         }
 
         @PostMapping
-        public ResponseEntity<Produto> post (@RequestBody Produto produto){
-            return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+        public ResponseEntity<Carrinho> post (@RequestBody Carrinho carrinho){
+            return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(carrinho));
         }
 
         @PutMapping
-        public ResponseEntity<Produto> put (@RequestBody Produto produto) {
-            return ResponseEntity.status(HttpStatus.OK).body(repository.save(produto));
+        public ResponseEntity<Carrinho> put (@RequestBody Carrinho carrinho) {
+            return ResponseEntity.status(HttpStatus.OK).body(repository.save(carrinho));
         }
 
         @DeleteMapping("/{id}")
