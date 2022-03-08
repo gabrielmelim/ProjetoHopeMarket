025e29d9c95c83994f76ee4c747e331f8bde09ca
Lucas Roberto de Oliveira Barcelos <lucasolbarcelos@gmail.com>
2022-03-08 09:59:29 -0300

    [@LucassRoberto Task: Criação da model UserLogin e UsuarioRepository]

---
+++ app_hopemarket/src/main/java/org/generation/app_hopemarket/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package org.generation.app_hopemarket.repository;
+
+import java.util.Optional;
+
+import org.generation.app_hopemarket.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	public Optional <Usuario> findByUsuario (String usuario);
+}
