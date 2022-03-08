e048405320a815fb17c73f0ebb08119e01847950
unknown <jeferson.aristaque@gmail.com>
2022-03-08 14:40:32 -0300

    @Aristaque[Task:Criando UserDetails e BasicSecurity]

--- app_hopemarket/src/main/java/org/generation/app_hopemarket/security/BasicSecurityConfig.java
+++ app_hopemarket/src/main/java/org/generation/app_hopemarket/security/BasicSecurityConfig.java
@@ -20,7 +20,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
             .antMatchers(HttpMethod.POST, "/api/usuario").permitAll()
-            .antMatchers(HttpMethod.PUT, "api/usuario/config").permitAll()
+            .antMatchers(HttpMethod.PUT, "/api/usuario/config").permitAll()
             .anyRequest().authenticated()
         .and().httpBasic()
         .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
